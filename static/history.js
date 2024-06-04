3d93d18222f9bd2a5561e4d90cb1c6a8e5b6b301
gustavobatata17 <133015470+gustavobatata17@users.noreply.github.com>
2024-06-04 10:31:24 -0300

    Add files via upload

--- static/history.js
+++ static/history.js
@@ -1,10 +1,10 @@
 let his_top1 = document.getElementById('his_top1')
-let his_top2= document.getElementById('his_top2')
+let his_top2 = document.getElementById('his_top2')
 let his_top3 = document.getElementById('his_top3')
-let his_top4= document.getElementById('his_top4')
-let his_top5= document.getElementById('his_top5')
-let his_top6= document.getElementById('his_top6')
-let his_top7= document.getElementById('his_top7')
+let his_top4 = document.getElementById('his_top4')
+let his_top5 = document.getElementById('his_top5')
+let his_top6 = document.getElementById('his_top6')
+let his_top7 = document.getElementById('his_top7')
 
 function des_his_top1() {
     if (his_top1.innerHTML == "") {
@@ -41,6 +41,14 @@ function des_his_top5(){
         his_top5.innerHTML = "";
     }
 }
+function des_his_top6(){
+    if (his_top6.innerHTML == "") {
+        his_top6.innerHTML = " <p>Estado Atual do LinkedIn em 2024</p><ul><li><strong>Crescimento de Usuários:</strong> Mais de 900 milhões de usuários em mais de 200 países.</li><li><strong>LinkedIn Learning:</strong> Plataforma robusta de educação online com milhares de cursos.</li><li><strong>Recrutamento e Talento:</strong> Ferramentas avançadas de recrutamento com inteligência artificial.</li><li><strong>LinkedIn Live e Eventos Virtuais:</strong> Transmissões ao vivo e eventos virtuais continuam a crescer.</li> <li><strong>Conteúdo e Engajamento:</strong> Aumento na produção e compartilhamento de conteúdo, com ferramentas de análise.</li><li><strong>LinkedIn Ads e Marketing B2B:</strong> Importante plataforma para marketing B2B com anúncios segmentados.</li><li><strong>Segurança e Privacidade:</strong> Investimentos contínuos para proteger dados e combater fraudes.</li><li><strong>Diversidade e Inclusão:</strong> Promoção ativa de discussões sobre diversidade e inclusão no local de trabalho.</li><li><strong>Integração com Microsoft:</strong> Integração contínua com ferramentas como Office 365 e Dynamics 365.</li> </ul>"
+    } else {
+        his_top6.innerHTML = "";
+    }
+}
+
 function des_his_top7(){
     if (his_top7.innerHTML == "") {
         his_top7.innerHTML = "<ul><li><strong>2012:</strong> Um grande ataque comprometeu cerca de 6,5 milhões de senhas de usuários, que foram posteriormente publicadas online.</li> <li><strong>2016:</strong> Foi revelado que a violação de 2012 era maior do que inicialmente divulgado, com mais de 167 milhões de contas afetadas.</li> <li><strong>2017:</strong> Hackers venderam dados de usuários do LinkedIn na dark web, expondo informações pessoais e profissionais.</li><li><strong>2021:</strong> Dados de 700 milhões de usuários foram coletados e vendidos online, incluindo informações extraídas de perfis públicos e combinadas com outros dados roubados.</li><li><strong>Segurança e Respostas:</strong> O LinkedIn tem investido em melhorias na segurança, implementando autenticação de dois fatores e sistemas avançados de monitoramento para detectar e prevenir atividades suspeitas.</li><li><strong>Dicas de Segurança:</strong><ul><li>Use senhas fortes e únicas para o LinkedIn.</li><li>Ative a autenticação de dois fatores.</li><li>Esteja atento a e-mails de phishing e mensagens suspeitas.</li></ul></li></ul>";
